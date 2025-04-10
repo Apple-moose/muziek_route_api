3d23292c5299afcdd9a8c88ce2b2c851910c0089
Apple-moose <patrissio@gmail.com>
2025-04-10 13:27:10 +0200

    new run command

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import uvicorn
+import subprocess
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=7000, reload=True)
+    subprocess.run([".venv/bin/uvicorn", "main:app", "--host", "127.0.0.1", "--port", "7000", "--reload"])
